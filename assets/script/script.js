985023686013a4d1ee69d6b96eb517ff25149c1e
Mike Rhiney <rhineyman@gmail.com>
2021-02-16 09:36:53 -0800

    initial build with ideas

---
+++ assets/script/script.js
@@ -0,0 +1,23 @@
+console.log("You linked that shit brah")
+
+// variables that call to id questions to input questions after prompting quiz start
+
+//event listeners for all buttons 
+//this includes answers start quiz access highscores?
+
+//conditional statements for correct and inccorect answers
+
+//statements that increase score/ decrease time based on boolean responses
+
+//create a timer for quiz. doesnt have to be special just has to work
+
+
+
+
+
+
+
+// function questionOne () {
+//     maybe we fill functions with questions that are callled on to fill parameters?
+//     no randomisation should have to happen 
+// }
