3a40f2a27680f811a000ceb207f7a0ca66c79d1a
Mike Rhiney <rhineyman@gmail.com>
2021-02-20 14:01:58 -0800

    points, timer, restart, highscore array

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,3 @@
-console.log("You linked that shit brah")
-
 // variables that call to id questions to input questions after prompting quiz start
 
 //event listeners for all buttons 
@@ -39,6 +37,11 @@ const startButton = document.getElementById("startBtn");
 const questionContainerElement = document.getElementById("questionContainer");
 const answerButton = document.getElementById("answerButtons");
 
+// change score to a # value
+
+var currentScore = 0
+var highName = ""
+var highScoreList = [""]
 
 startButton.addEventListener("click", startQuiz);
 
@@ -46,180 +49,184 @@ startButton.addEventListener("click", startQuiz);
 let shuffledQuestions, currentQuestionIndex
 
 function startQuiz() {
-    Timer();
-    scoreReset();
+  Timer();
+  scoreReset();
 
-    startButton.classList.add('hide')
+  startButton.classList.add('hide')
   shuffledQuestions = questions.sort(() => Math.random() - .5)
   currentQuestionIndex = 0
-  
-  setNextQuestion()   
-  
+
+  setNextQuestion()
+
 }
 
 
 function scoreReset() {
-    var scoreStart = 0
-    scoreElement = document.getElementById("score");
-    scoreElement.innerHTML = scoreStart;    
+  var scoreStart = 0
+  currentScore = scoreStart;
+  scoreElement = document.getElementById("score");
+  scoreElement.innerHTML = currentScore;
+    
 }
 
 
 function Timer() {
-    var startCount = 50;
-    const stopCount = 0,
-        duration = 10000,
-        countDownElement = document.getElementById('timer'),
-        intervalTime = duration / Math.abs(startCount - stopCount);
+  var startCount = 50;
+  const stopCount = 0,
+    duration = 10000,
+    countDownElement = document.getElementById('timer'),
+    intervalTime = duration / Math.abs(startCount - stopCount);
 
-    var countDown = setInterval(
-        function () {
-            if (startCount === stopCount) clearInterval(countDown)
+  var countDown = setInterval(
+    function () {
+      if (startCount === stopCount) clearInterval(countDown)
 
-            countDownElement.innerHTML = startCount;
-            if (startCount > stopCount) {
-                startCount--
-            } else {
-              startButton.innerText = 'Restart';
-              startButton.classList.remove('hide');
-              var highscore = prompt("Enter your initials"); 
-            }
-        },
-        intervalTime       
-    );
+      countDownElement.innerHTML = startCount;
+      if (startCount > stopCount) {
+        startCount--
+      } else {
+        startButton.innerText = 'Restart';
+        startButton.classList.remove('hide');
+        var highscoreName = prompt("Enter your initials");        
+        highName = highscoreName;
+        
+
+        resetState();
+      }
+    },
+    intervalTime
+  );
 }
 
 
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    button.addEventListener('click', selectAnswer)
+    answerButton.appendChild(button)
 
+  })
+}
 
 function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-  }
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function selectAnswer() {
+  currentScore++;
+  scoreElement = document.getElementById("score");
+  scoreElement.innerHTML = currentScore;
   
-  function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-      const button = document.createElement('button')
-      button.innerText = answer.text
-      button.classList.add('btn')      
-      button.addEventListener('click', selectAnswer)
-      answerButton.appendChild(button)
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    setNextQuestion();
 
-    })
+  } else {
+    startButton.innerText = 'Restart';
+    startButton.classList.remove('hide');
   }
-
-
-function selectAnswer() {        
-    
-    if (shuffledQuestions.length > currentQuestionIndex + 1) { 
-        shuffledQuestions = questions.sort(() => Math.random() - .5) 
-        setNextQuestion()
-  
-} else {     
-      startButton.innerText = 'Restart';
-      startButton.classList.remove('hide');    
-  }  
+  console.log(currentScore);
 }
 
 
 
 function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-      element.classList.add('correct')
-    } else {
-      element.classList.add('wrong')
-    }
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
   }
-  
+}
 
-  function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+function resetState() {
+  while (answerButton.firstChild) {
+    answerButton.removeChild(answerButton.firstChild)
   }
-
-
-function resetState() {   
-    
-    while (answerButton.firstChild) {
-      answerButton.removeChild(answerButton.firstChild)
-    }
-  }
+}
 
 
 
 const questions = [
-    {
-        question: 'What is 2 + 2?',
-        answers: [
-            { text: '4', correct: true },
-            { text: '6', correct: false },
-            { text: '8', correct: false },
-            { text: '22', correct: false }
-        ]
-    },
-    {
-        question: 'Which command will log array length',
-        answers: [
-            { text: 'console.log(array.length)', correct: true },
-            { text: 'console.array.length', correct: false },
-            { text: 'length.array', correct: false },
-            { text: 'array.length', correct: false }
-        ]
-    },
-    {
-        question: 'Is web development fun?',
-        answers: [
-            { text: 'Kinda', correct: false },
-            { text: 'YES!!!', correct: true },
-            { text: 'no', correct: false },
-            { text: 'IDK', correct: false }
-        ]
-    },
-    {
-        question: 'Which school is best?',
-        answers: [
-            { text: 'OSU', correct: false },
-            { text: 'OU', correct: true },
-            { text: 'UofA', correct: false },
-            { text: 'UT', correct: false }
-        ]
-    },
-    {
-      question: '5 * 5',
-      answers: [
-          { text: '6', correct: false },
-          { text: '25', correct: true },
-          { text: '4', correct: false },
-          { text: '22', correct: false }
-      ]
-   },
-   {
-    question: '3 + 3',
+  {
+    question: 'What is 2 + 2?',
     answers: [
-        { text: '6', correct: false },
-        { text: '3', correct: true },
-        { text: '4', correct: false },
-        { text: '22', correct: false }
+      { text: '4', correct: true },
+      { text: '6', correct: false },
+      { text: '8', correct: false },
+      { text: '22', correct: false }
     ]
   },
   {
-  question: 'x = 3 + 3, x=',
-  answers: [
-      { text: '8', correct: false },
-      { text: '6', correct: true },
-      { text: '4', correct: false },
-      { text: '22', correct: false }
-  ]
+    question: 'Which command will log array length',
+    answers: [
+      { text: 'console.log(array.length)', correct: true },
+      { text: 'console.array.length', correct: false },
+      { text: 'length.array', correct: false },
+      { text: 'array.length', correct: false }
+    ]
   },
   {
-  question: '11 * 2',
-  answers: [
+    question: 'Is web development fun?',
+    answers: [
+      { text: 'Kinda', correct: false },
+      { text: 'YES!!!', correct: true },
+      { text: 'no', correct: false },
+      { text: 'IDK', correct: false }
+    ]
+  },
+  {
+    question: 'Which school is best?',
+    answers: [
+      { text: 'OSU', correct: false },
+      { text: 'OU', correct: true },
+      { text: 'UofA', correct: false },
+      { text: 'UT', correct: false }
+    ]
+  },
+  {
+    question: '5 * 5',
+    answers: [
+      { text: '16', correct: false },
+      { text: '25', correct: true },
+      { text: '23', correct: false },
+      { text: '50', correct: false }
+    ]
+  },
+  {
+    question: '3 + 3',
+    answers: [
       { text: '6', correct: false },
+      { text: '32', correct: true },
+      { text: '14', correct: false },
+      { text: '222', correct: false }
+    ]
+  },
+  {
+    question: 'x = 3 + 3, x=',
+    answers: [
       { text: '8', correct: false },
-      { text: '4', correct: false },
+      { text: '6', correct: true },
+      { text: '42', correct: false },
+      { text: '212', correct: false }
+    ]
+  },
+  {
+    question: '11 * 2',
+    answers: [
+      { text: '16', correct: false },
+      { text: '18', correct: false },
+      { text: '14', correct: false },
       { text: '22', correct: true }
-  ]
+    ]
   },
 ]
